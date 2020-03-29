9f403ed76195da998009273ac0147d44d7442110
Jakub Kozera <jakub@kozera.pl>
2020-03-29 08:32:59 +0200

    first upload
    Signed-off-by: Jakub Kozera <jakub@kozera.pl>

---
+++ cw3/cw3/cw3/Controllers/StudentsController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using cw3.DAL;
+using cw3.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw3.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        /*
+        public IActionResult Index()
+        {
+            return View(); 
+        }
+        */
+
+        /*
+        [HttpGet]
+        public string GetStudent()
+        {
+            return "Kowalski, Malewski, Andrzejewski";
+        }
+        */
+
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dBservice)
+        {
+            _dbService = dBservice;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+
+        /*
+        [HttpGet]
+        public string GetStudent(string orderBy)
+        {
+            return $"Kowalski, Malewski, Andrzejewski sortowanie = {orderBy}";
+        }
+        */
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            } else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+
+            return NotFound("Nie znaleziono studenta.");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            //... add to database
+            //... generating index number
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutStudent(string id)
+        {
+            return Ok("Aktualizacja dokończona");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(string id)
+        {
+            return Ok("Usuwanie ukończone");
+        }
+
+    }
+}
